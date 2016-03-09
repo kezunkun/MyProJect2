5c9fbb9da85027a855ac3f5a057125659bbecff6
kezunkun <948226842@qq.com>
2016-03-09 20:06:45 +0800

    Initial commit

---
+++ myapp1/src/main/java/com/example/ke/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ke.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //你妹的
+        //加一行
+    }
+}
