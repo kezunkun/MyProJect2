a1808954bf52ef17b2f142e33ff171ca81145595
kezunkun <948226842@qq.com>
2016-03-09 20:09:28 +0800

    第一次提交代码

--- app/src/main/java/com/myproject1/ke/myproject1/MainActivity.java
+++ app/src/main/java/com/myproject1/ke/myproject1/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //加一行代码
+        //加第二行代码
 
     }
 }
